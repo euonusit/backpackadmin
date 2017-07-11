0e08d610cd59b862f1dd3a07e057fd385a96d323
euonusit <dinesh.euonus@gmail.com>
2017-07-11 09:08:46 +0530

    cjang

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -5,7 +5,7 @@
 .about ul { text-align: justify !important; }
 
 </style>
-<div class="banner-1 asdasd as dasasd as">
+<div class="banner-1">
 	<div class="container">
 		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">{{$page->title}}</h1>
 	</div>
